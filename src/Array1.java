1dc88fca0c32eb16ee1ba7bd83e7a5434dde4cd8
AlinaLFox <124040276+AlinaLFox@users.noreply.github.com>
2023-02-14 21:34:37 +0200

    Initial commit

---
+++ src/Array1.java
@@ -0,0 +1,9 @@
+public class Array1 {
+    public static void main(String[] args) {
+
+        int price[] = new int[] {100, 200, 300, 400, 500};
+        for (int value = 0; value < price.length; value++) {
+            System.out.print(price[value] + " ");
+        }
+    }
+}
